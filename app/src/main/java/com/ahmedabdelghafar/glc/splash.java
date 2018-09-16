831b44754c60385d8de053187c0c7a78fb2ad1e3
a.abdelghafar <a.abdelghafar@AHMED-ABDELGHAF>
2018-09-16 14:19:18 +0200

    v3

--- app/src/main/java/com/ahmedabdelghafar/glc/splash.java
+++ app/src/main/java/com/ahmedabdelghafar/glc/splash.java
@@ -1,13 +1,26 @@
 package com.ahmedabdelghafar.glc;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.security.KeyStore;
+
 public class splash extends AppCompatActivity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainintent = new Intent(splash.this, Login.class);
+                splash.this.startActivity(mainintent);
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
+
 }
